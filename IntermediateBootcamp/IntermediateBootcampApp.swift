b315384e5ba385f61bd6d08fdac0e4207c32c8d9
DJ Cartwright <dj.cartwright.web@gmail.com>
2024-12-02 12:09:46 -0500

    Initial Commit

---
+++ IntermediateBootcamp/IntermediateBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntermediateBootcampApp.swift
+//  IntermediateBootcamp
+//
+//  Created by David Cartwright on 2024-12-02.
+//
+
+import SwiftUI
+
+@main
+struct IntermediateBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
