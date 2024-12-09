558da40038c9994a75e5244c5448a7de41e01c7b
DJ Cartwright <dj.cartwright.web@gmail.com>
2024-12-09 13:59:12 -0500

    All work done to set up project and first real commit...hope this goes well

--- IntermediateBootcamp/MultipleSheetsView.swift
+++ IntermediateBootcamp/MultipleSheetsView.swift
@@ -7,9 +7,45 @@
 
 import SwiftUI
 
+struct RandomModel: Identifiable {
+    let id = UUID().uuidString
+    
+    let title: String
+}
+
+
+
 struct MultipleSheetsView: View {
+    
+    @State var selectedModel: RandomModel? = nil
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(spacing: 20) {
+                
+                ForEach(0..<50) { index in
+                    Button("Button \(index)") {
+                        selectedModel = RandomModel(title: "\(index)")
+                    }
+                }
+                
+                
+                
+            }
+            .sheet(item: $selectedModel) { model in
+                NextScreen(selectedModel: model)
+            }
+        }
+    }
+}
+
+struct NextScreen: View {
+    
+    let selectedModel: RandomModel
+    
+    var body: some View {
+        Text(selectedModel.title)
+            .font(.largeTitle)
     }
 }
 
