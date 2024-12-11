3e8f1bd6c9ef0c40ff02e82aa36f34c92213afea
DJ Cartwright <dj.cartwright.web@gmail.com>
2024-12-11 03:35:40 -0500

    complete video #8, mask

--- IntermediateBootcamp/MaskView.swift
+++ IntermediateBootcamp/MaskView.swift
@@ -8,9 +8,47 @@
 import SwiftUI
 
 struct MaskView: View {
+    
+    @State var rating: Int = 2
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            StarsView
+                .overlay {
+                    OverlayView
+                        .mask(StarsView)
+                }
+        }
     }
+    
+    private var OverlayView: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle()
+//                    .foregroundStyle(.yellow)
+                    .fill(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing))
+                    .frame(width: CGFloat(rating) / 5 * geometry.size.width)
+            }
+        }
+        .allowsHitTesting(false) //without, unable to tap as tapping on overlayview and not starsview
+    }
+    
+    private var StarsView: some View {
+        HStack {
+            ForEach(1..<6) { index in
+                Image(systemName: "star.fill")
+                    .font(.largeTitle)
+                    .foregroundStyle(.gray)
+                    .onTapGesture {
+                        withAnimation(.easeInOut) {
+                            rating = index
+                        }
+                    }
+            }
+            
+        }
+    }
+    
 }
 
 #Preview {
